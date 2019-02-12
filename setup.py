c7f677ca666e78639f5a7e36d8938df4d5c62818
bhujay <bhujay.bhatta@yahoo.com>
2019-02-12 19:55:34 +0530

    renamimg

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
-setup(name='tokenleader_client',
-      version='0.1',
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='tokenleaderclient',
+      version='0.3',
       description='Client for token based authentication and role based access control',
+      long_description=readme(),
       url='https://github.com/microservice-tsp-billing/tokenleader-client',
       author='Bhujay Kumar Bhatta',
       author_email='bhujay.bhatta@yahoo.com',
       license='MIT',
-      packages=['tokenleader-client'],
+      packages=['tokenleaderclient'],
+      install_requires=[
+          'markdown',
+          'PyJWT==1.7.0',
+          'PyYAML==3.13',
+      ],
+
       zip_safe=False)
