ccc4b3770df9abcd7f7e753e7c5ad94f70f68713
project <project@project-PC>
2019-03-22 15:17:33 +0530

    k

--- tokenleaderclient/client/cli_parser.py
+++ tokenleaderclient/client/cli_parser.py
@@ -118,7 +118,7 @@ def main():
     if  sys.argv[1] == 'list':
         if options.entity == 'ou':
           if options.name:
-            print(c.list_ouX_byname(options.name))
+            print(c.list_ou_byname(options.name))
           else:
             print(c.list_ou())
             
