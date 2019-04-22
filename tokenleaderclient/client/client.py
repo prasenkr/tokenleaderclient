3b845b0c8f2ec976f791a2249fbc16136ee1c0e6
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-22 18:49:56 +0530

    v

--- tokenleaderclient/client/client.py
+++ tokenleaderclient/client/client.py
@@ -107,19 +107,22 @@ class Client():
             r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)
             r_dict = json.loads(r.content.decode())   
         except Exception as e:
-            r_dict = {'error': 'could not conect to server , the error is {}'.format(e)}    #     
+            r_dict = {'error': 'could not conect to server , the error is {}'.format(e)}       
         
-#         print(r_dict)  # for displaying from the cli  print in cli parser
+#       print(r_dict)  # for displaying from the cli  print in cli parser
         return r_dict
         
     def list_org(self):
         token = self.get_token().get('auth_token')
         api_route = '/list/org'
         service_endpoint = self.tl_url + api_route
-        headers={'X-Auth-Token': token}    
-        r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)       #     
-        r_dict = json.loads(r.content.decode())
-        print(r)  # for displaying from the cli  print in cli parser
+        headers={'X-Auth-Token': token}   
+        try:
+            r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)           
+            r_dict = json.loads(r.content.decode())
+        except Exception as e:
+        	r_dict = {'error': 'could not conect to server , the error is {}'.format(e)}            
+            #print(r)  # for displaying from the cli  print in cli parser
         return r_dict
 
     def list_dept(self):
