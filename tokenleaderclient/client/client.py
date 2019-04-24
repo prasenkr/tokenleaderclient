38dd506285af332a6869a42fe6e94d50d052645f
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-24 17:47:51 +0530

    list_wfc added

--- tokenleaderclient/client/client.py
+++ tokenleaderclient/client/client.py
@@ -121,10 +121,18 @@ class Client():
             r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)           
             r_dict = json.loads(r.content.decode())
         except Exception as e:
-        	r_dict = {'error': 'could not conect to server , the error is {}'.format(e)}            
-            
+        	r_dict = {'error': 'could not conect to server , the error is {}'.format(e)}        
         return r_dict
-
+        	    
+	def list_wfc(self):
+        token = self.get_token().get('auth_token')
+        api_route = '/list/wfc'
+        service_endpoint = self.tl_url + api_route
+        headers={'X-Auth-Token': token}
+        r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)
+        r_dict = json.loads(r.content.decode())
+        return r_dict
+        		
     def list_dept(self):
         token = self.get_token().get('auth_token')
         api_route = '/list/dept'
