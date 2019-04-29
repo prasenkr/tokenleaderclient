1461281760892f5f46297c253ecd9fa85991b173
project <project@project-PC>
2019-04-29 12:40:24 +0530

    list_wfc

--- tokenleaderclient/client/client.py
+++ tokenleaderclient/client/client.py
@@ -120,6 +120,15 @@ class Client():
 #         print(r_dict)  # for displaying from the cli  print in cli parser
         return r_dict
 
+    def list_wfc(self):
+        token = self.get_token().get('auth_token')
+        api_route = '/list/wfc'
+        service_endpoint = self.tl_url + api_route
+        headers={'X-Auth-Token': token}    
+        r = requests.get(service_endpoint, headers=headers, verify=self.ssl_verify)       #     
+        r_dict = json.loads(r.content.decode())
+        return r_dict
+
     def list_dept(self):
         token = self.get_token().get('auth_token')
         api_route = '/list/dept'
