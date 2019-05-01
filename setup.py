8f8b3f519e2645f743ead2ed069faf543198a868
project <project@project-PC>
2019-05-01 18:22:52 +0530

    b

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='tokenclient',
-      version='0.3',
+      version='0.6',
       description='Client for token based authentication and role based access control',
       long_description=readme(),
       url='https://github.com/prasenkr/tokenleaderclient.git',
